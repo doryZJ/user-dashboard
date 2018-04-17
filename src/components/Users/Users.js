ed4817c3451d0e4ee307b11622aeefb851c06a59
doryZJ <2298635139@qq.com>
2018-04-17 17:48:17 +0800

    feat(dashboard): add users

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,59 +1,61 @@
 import React from 'react';
+import { connect } from 'dva';
 import { Table, Pagination, Popconfirm } from 'antd';
 import styles from './Users.css';
+import { PAGE_SIZE } from '../../components';
 
-const columns = [{
-  title: 'Name',
-  dataIndex: 'name',
-}, {
-  title: 'Email',
-  dataIndex: 'email',
-}, {
-  title: 'Website',
-  dataIndex: 'website',
-}, {
-  title: 'Operation',
-  render: (text, { id }) => (
-    <span className="styles.operation">
-      <a href="">Edit</a>
-      <Popconfirm title="Confirm to delete?" onConfirm={deleteHandler.bind(null, id)}>
-        <a href="">Delete</a>
-      </Popconfirm>
-    </span>
-  ),
-},
-];
+function Users({ list: dataSource, total, page: current }) {
+  function deleteHandler(id) {
+    console.warn(`TODO: ${id}`);
+  }
+  const columns = [{
+    title: 'Name',
+    dataIndex: 'name',
+  }, {
+    title: 'Email',
+    dataIndex: 'email',
+  }, {
+    title: 'Website',
+    dataIndex: 'website',
+  }, {
+    title: 'Operation',
+    render: (text, { id }) => (
+      <span className="styles.operation">
+        <a href="">Edit</a>
+        <Popconfirm title="Confirm to delete?" onConfirm={deleteHandler.bind(null, id)}>
+          <a href="">Delete</a>
+        </Popconfirm>
+      </span>
+    ),
+  },
+  ];
 
-const data = [{
-  key: '1',
-  name: 'Leanne Graham',
-  email: 'Sincere@april.biz',
-  website: 'hildegard.org',
-}, {
-  key: '2',
-  name: 'Ervin Howell',
-  email: 'Shanna@melissa.tv',
-  website: 'anastasia.net',
-}, {
-  key: '3',
-  name: 'Clementine Bauch',
-  email: 'Nathan@yesenia.net',
-  website: 'ramiro.info',
-},
-];
-
-function Users() {
   return (
     <div className={styles.normal}>
       <div>
         <Table
           columns={columns}
-          dataSource={data}
+          dataSource={dataSource}
           Pagination={false}
+        />
+        <Pagination
+          className="ant-table-pagination"
+          total={total}
+          current={current}
+          pageSize={PAGE_SIZE}
         />
       </div>
     </div>
   );
 }
 
-export default Users;
+function mapStateToProps(state) {
+  const { list, total, page } = state.users;
+  return {
+    list,
+    total,
+    page,
+  };
+}
+
+export default connect(mapStateToProps)(Users);
